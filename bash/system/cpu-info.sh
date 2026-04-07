579380e2a214d9f5f88bb9dad964721919ae4bac
RJ23 <106665114+rj23dev@users.noreply.github.com>
2026-04-07 20:47:59 +0000

    Fix trimming logic and rename coresPerSockets to coresPerSocket

--- bash/system/cpu-info.sh
+++ bash/system/cpu-info.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
-lscpu | awk -F: ' $1=="Model name" {
-		       gsub(/^[ \t]+|[ \t]+$/, "", $2); 
-		       model=$2 
-		  }
-	          $1=="CPU(s)" {
-		       gsub(/^[ \t]+|[ \t]+$/, "", $2); 
-		       threads=$2 
-		  }
-		  $1=="Core(s) per socket" { 
-		       sub(/^[ \t]+|[ \t]+$/, "", $2); 
-		       coresPerSockets=$2 
-		  }
-		  $1=="Socket(s)" {
-		       gsub(/^[ \t]+|[ \t]+$/, "", $2); 
-		       sockets=$2 
-		  }
+lscpu | awk -F: '$1=="Model name" {
+		       		gsub(/^[ \t]+|[ \t]+$/, "", $2);
+		       		model=$2
+		  	}
+	         $1=="CPU(s)" {
+		       		gsub(/^[ \t]+|[ \t]+$/, "", $2);
+		       		threads=$2
+		  	}
+		 $1=="Core(s) per socket" {
+		       		gsub(/^[ \t]+|[ \t]+$/, "", $2);
+		       		coresPerSocket=$2
+		  	}
+		 $1=="Socket(s)" {
+		       		gsub(/^[ \t]+|[ \t]+$/, "", $2);
+		       		sockets=$2
+		  	}
 
-		END {
-			cores = coresPerSockets * sockets 
+		 END {
+			cores = coresPerSocket * sockets
 			printf "Model: %s\nThreads: %s\nCores: %d\n", model, threads, cores
-}
-'
+		}
+		'
