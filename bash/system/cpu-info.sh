cd089d2b1026be91ec8c13107f15aa45e538b5d5
RJ23 <106665114+rj23dev@users.noreply.github.com>
2026-04-07 21:01:50 +0000

    Improve indentation and remove unnecessary semicolons

--- bash/system/cpu-info.sh
+++ bash/system/cpu-info.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
-lscpu | awk -F: '$1=="Model name" {
-		       		gsub(/^[ \t]+|[ \t]+$/, "", $2);
-		       		model=$2
-		  	}
-	         $1=="CPU(s)" {
-		       		gsub(/^[ \t]+|[ \t]+$/, "", $2);
-		       		threads=$2
-		  	}
-		 $1=="Core(s) per socket" {
-		       		gsub(/^[ \t]+|[ \t]+$/, "", $2);
-		       		coresPerSocket=$2
-		  	}
-		 $1=="Socket(s)" {
-		       		gsub(/^[ \t]+|[ \t]+$/, "", $2);
-		       		sockets=$2
-		  	}
-
-		 END {
-			cores = coresPerSocket * sockets
-			printf "Model: %s\nThreads: %s\nCores: %d\n", model, threads, cores
-		}
-		'
+lscpu | awk -F: '
+$1=="Model name" {
+	gsub(/^[ \t]+|[ \t]+$/, "", $2)
+	model=$2
+}
+$1=="CPU(s)" {
+	gsub(/^[ \t]+|[ \t]+$/, "", $2)
+	threads=$2
+}
+$1=="Core(s) per socket" {
+	gsub(/^[ \t]+|[ \t]+$/, "", $2)
+	coresPerSocket=$2
+}
+$1=="Socket(s)" {
+	gsub(/^[ \t]+|[ \t]+$/, "", $2)
+	sockets=$2
+}  
+END {
+	cores = coresPerSocket * sockets
+	printf "Model: %s\nThreads: %s\nCores: %d\n", model, threads, cores
+}
+'
